04c36aa030f84da9608ecff11cec08f834a4ecdd
Anadi Dev Soni <anadidevsoni2005@gmail.com>
2026-06-30 14:12:33 +0530

    1358-number-of-substrings-containing-all-three-characters

--- LeetCode/Medium/1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ LeetCode/Medium/1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
+    // int numberOfSubstrings(string s) {
+    //     vector<int> freq={0,0,0};
+    //     int l=0,r=0;
+    //     int count=0;
+    //     while(r<s.length()){
+    //         if (s[r]=='a') freq[0]++;
+    //         if (s[r]=='b') freq[1]++;
+    //         if (s[r]=='c') freq[2]++;
+    //         while(freq[0]>=1 &&freq[1]>=1 && freq[2]>=1){
+    //             if (s[l]=='a') freq[0]--;
+    //             if (s[l]=='b') freq[1]--;
+    //             if (s[l]=='c') freq[2]--;
+    //             l++;
+    //         }
+    //         count+=l;
+    //         r++;
+    //     }
+    //     return count;
+    // }
     int numberOfSubstrings(string s) {
-        vector<int> freq={0,0,0};
-        int l=0,r=0;
-        int count=0;
-        while(r<s.length()){
-            if (s[r]=='a') freq[0]++;
-            if (s[r]=='b') freq[1]++;
-            if (s[r]=='c') freq[2]++;
-            while(freq[0]>=1 &&freq[1]>=1 && freq[2]>=1){
-                if (s[l]=='a') freq[0]--;
-                if (s[l]=='b') freq[1]--;
-                if (s[l]=='c') freq[2]--;
-                l++;
+        int last[3] = {-1, -1, -1}, left = 0, count = 0;
+        for(int right = 0; right < s.size(); right++){
+            char c = s[right];
+            last[c - 'a'] = right;
+            if(last[0] != -1 && last[1] != -1 && last[2] != -1){
+                count += min({last[0], last[1], last[2]}) + 1;
             }
-            count+=l;
-            r++;
         }
+
         return count;
     }
 };
