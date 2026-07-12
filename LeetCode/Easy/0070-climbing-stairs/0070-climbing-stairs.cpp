ab8cd10f97306707bd536b156efd7de0ee1357f6
Anadi Dev Soni <anadidevsoni2005@gmail.com>
2026-07-12 13:37:29 +0530

    0070-climbing-stairs

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.cpp
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    void solve(int n, vector<int>& dp) {
-        if (dp[n] != -1) return;
-        if (n == 0) { dp[n] = 1; return; }
-        if (n == 1) { dp[n] = 1; return; }
-        solve(n - 1, dp);
-        solve(n - 2, dp);
-        dp[n] = dp[n - 1] + dp[n - 2];
-    }
+    // void solve(int n, vector<int>& dp) {
+    //     if (dp[n] != -1) return;
+    //     if (n == 0) { dp[n] = 1; return; }
+    //     if (n == 1) { dp[n] = 1; return; }
+    //     solve(n - 1, dp);
+    //     solve(n - 2, dp);
+    //     dp[n] = dp[n - 1] + dp[n - 2];
+    // }
 
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
-        solve(n, dp);
+        dp[0]=1;dp[1]=1;
+        // solve(n, dp);
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
 };
