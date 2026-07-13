6cd10a7a2b971eacc129757bd84bda18084e602b
Anadi Dev Soni <anadidevsoni2005@gmail.com>
2026-07-13 16:41:27 +0530

    0064-minimum-path-sum

--- LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -4,10 +4,10 @@ public:
     int dp[201][201];
     vector<vector<int>> dir={{0,1},{1,0}};
     int memo(int r,int c,vector<vector<int>>& grid){
-        if(r>=m||c>=n) return INT_MAX-10;
+        if(r>=m||c>=n) return INT_MAX-200;
         if (r==m-1 && c==n-1) return grid[r][c];
         if(dp[r][c]!=-1) return dp[r][c];
-        int minpath=INT_MAX-10;
+        int minpath=INT_MAX-200;
         for(const auto &x:dir){
             int nr=r+x[0],nc=c+x[1];
             minpath=min(minpath,memo(nr,nc,grid));
@@ -26,11 +26,27 @@ public:
         }
         return dp[m-1][n-1];
     }
+    int space_opt(vector<vector<int>>& grid){
+        vector<int> prev (n,0);
+        prev[0] = grid[0][0];
+        prev[0]=grid[0][0];
+        for(int i=1;i<n;i++) prev[i]=prev[i-1]+grid[0][i];
+        for(int i=1;i<m;i++){
+            vector<int> temp(n);
+            temp[0]=prev[0]+grid[i][0];
+            for(int j=1;j<n;j++){
+                temp[j] = grid[i][j] + min(temp[j-1], prev[j]);
+            }
+            prev=temp;
+        }
+        return prev[n-1];
+    }
     int minPathSum(vector<vector<int>>& grid) {
         m=grid.size(),n=grid[0].size();
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         // return memo(0,0,grid);
-        return tab(grid);
+        // return tab(grid);
         // return dp[m-1][n-1];
+        return space_opt(grid);
     }
 };
