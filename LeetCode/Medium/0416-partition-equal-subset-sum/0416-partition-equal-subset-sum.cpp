218394694169207ad064968fcd0a405755f75ed9
Anadi Dev Soni <anadidevsoni2005@gmail.com>
2026-07-14 22:20:31 +0530

    0416-partition-equal-subset-sum

---
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& arr) {
+        int n=arr.size();
+        int total_sum=accumulate(arr.begin(),arr.end(),0);
+        if(total_sum&1) return false;
+        vector<bool> dp(total_sum+1,0);
+        int target =0;
+        dp[0]=true;
+        if(arr[0]<=total_sum){
+            dp[arr[0]]=true;
+        }
+        for(int i=1;i<n;i++){
+            vector<bool> crr(total_sum+1,0);
+            for(int t=1;t<=total_sum;t++){
+                bool included = (t-arr[i]>=0)? dp[t-arr[i]]:false;
+                bool excluded = dp[t];
+                crr[t]=included || excluded;
+            }
+            dp=crr;
+        }
+        return dp[total_sum/2];;
+    }
+};
