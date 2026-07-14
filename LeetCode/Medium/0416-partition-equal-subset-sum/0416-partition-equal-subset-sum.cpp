246ff7c3c68f645ac459c0ac99628fdd4c6ec269
Anadi Dev Soni <anadidevsoni2005@gmail.com>
2026-07-14 22:21:45 +0530

    0416-partition-equal-subset-sum

--- LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -5,7 +5,7 @@ public:
         int total_sum=accumulate(arr.begin(),arr.end(),0);
         if(total_sum&1) return false;
         vector<bool> dp(total_sum+1,0);
-        int target =0;
+        int target =total_sum/2;
         dp[0]=true;
         if(arr[0]<=total_sum){
             dp[arr[0]]=true;
