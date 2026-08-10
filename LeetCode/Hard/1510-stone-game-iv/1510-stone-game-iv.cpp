e81fe0a2ff3502d0b908b4db24c5ef1e1a1fdf1e
Anadi Dev Soni <anadidevsoni2005@gmail.com>
2026-08-11 01:23:41 +0530

    1510-stone-game-iv

---
+++ LeetCode/Hard/1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dp;
+    int f(int n){
+        if(n<=0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        for(int i=1;i*i<=n;i++){
+            if(f(n-(i*i))==0) return dp[n]=1;
+        }
+        return dp[n]=0;
+    }
+    bool winnerSquareGame(int n) {
+        dp.assign(n+1,-1);
+        return f(n);
+    }
+};
