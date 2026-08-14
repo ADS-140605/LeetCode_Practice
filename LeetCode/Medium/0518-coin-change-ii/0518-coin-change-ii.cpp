98ec6d9c06b2fd70c598db31587211a94fecb3e4
Anadi Dev Soni <anadidevsoni2005@gmail.com>
2026-08-15 02:32:09 +0530

    0518-coin-change-ii

--- LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
+++ LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[301][5001];
+    long long dp[301][5001];
     int solve(int i,int a, vector<int>& c){
         if(a==0) return 1;
         if(i>=c.size()) return 0;
@@ -11,6 +11,14 @@ public:
     }
     int change(int a, vector<int>& c) {
         memset(dp,-1,sizeof(dp));
+        vector<unsigned long long> odp(a+1,0);
+        odp[0]=1;
+        for(int i=c.size()-1;i>=0;i--){
+            for(int j=c[i];j<=a;j++){
+                odp[j]+=odp[j-c[i]];
+            }
+        }
+        return odp[a];
         return solve(0,a,c);
     }
 };
